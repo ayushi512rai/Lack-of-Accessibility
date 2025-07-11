cc7c1cd3b627332aa107bf0c52bf061b4a377f26
ayushi512rai <ayushi512rai@gmail.com>
2025-07-11 13:15:01 +0530

    Add files via upload

---
+++ install-extensions.bat
@@ -0,0 +1,19 @@
+@echo off
+REM Install VS Code extensions for AI Accessibility Suite project
+
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension esbenp.prettier-vscode
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension christian-kohler.path-intellisense
+code --install-extension eamodio.gitlens
+code --install-extension humao.rest-client
+code --install-extension waderyan.nodejs-extension-pack
+code --install-extension Microsoft.accessibility-insights-web
+code --install-extension naumovs.color-highlight
+code --install-extension gencay.vscode-ai-doc-writer
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-python.python
+
+echo All recommended VS Code extensions have been installed.
+pause 
